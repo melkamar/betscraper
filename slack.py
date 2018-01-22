fc075cae6993439ec91dbb359bee8157d30f7b96
Martin Melka <melkamar@fit.cvut.cz>
2018-01-22 23:43:32 +0100

    wip

--- slack.py
+++ slack.py
@@ -1,9 +1,7 @@
 import requests
 
 
-def send_message(text):
-    webhook_url = r'https://hooks.slack.com/services/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'
-
+def send_message(text, webhook_url):
     payload = {
         "text": text,
         "icon_emoji": ':ice_hockey_stick_and_puck:',
