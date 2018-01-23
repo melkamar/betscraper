0b5e6dfacfc267c88f30cd671dd6e9ac387ecca5
Martin Melka <martin.melka@gmail.com>
2018-01-23 20:31:26 +0100

    wip

--- run-betscraper.sh
+++ run-betscraper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cd /root/betscraper
-git pull
+while true; do
+	cd /root/betscraper 
+	git pull >> /var/log/betscraper.log 2>>/var/log/betscraper.log
 
-cd /root/
-/usr/local/bin/docker-compose run betscraper
+	cd /root/
+	/usr/local/bin/docker-compose run betscraper  >> /var/log/betscraper.log 2>>/var/log/betscraper.log
+
+	sleep 2
+done
